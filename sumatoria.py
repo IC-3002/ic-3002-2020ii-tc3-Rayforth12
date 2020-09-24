5affd9aeb56c23685e56ed2d54f50a163ede974c
Rayforth12 <Rayforth1616@estudiantec.cr>
2020-09-23 22:29:55 -0600

    cambios para la tarea 3

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,16 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+    for i in range(n):
+    	for j in range(i):
+    	    n=i
+    	    k=j
+    	    for k in range(j+n):
+    	    	return 1
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
-
+    for i in range(n):
+    	for j in range(i):
+    	    n=i
+    	    k=j
+    	    for k in range(j+n):
+    	    	return 1
+    
